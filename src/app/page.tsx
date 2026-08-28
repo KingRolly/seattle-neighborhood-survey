c7c4b4c80973e352a4a93b7f128c8efce57d752b
Jack Peters <kingrolly2006@gmail.com>
2026-08-27 23:48:25 -0700

    Added blocks to map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect, useRef } from "react";
-import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 
 export default function Home() {
@@ -19,7 +18,35 @@ export default function Home() {
         container: mapContainer.current,
         style: "https://tiles.openfreemap.org/styles/liberty",
         center: [-122.3321, 47.6062],
-        zoom: 10,
+        zoom: 12,
+      });
+
+      map.on("load", () => {
+        map.addSource("seattle-blocks", {
+          type: "geojson",
+          data: "/seattle-blocks.geojson",
+        });
+
+        map.addLayer({
+          id: "seattle-block-fills",
+          type: "fill",
+          source: "seattle-blocks",
+          paint: {
+            "fill-color": "#3388ff",
+            "fill-opacity": 0.15,
+          },
+        });
+
+        map.addLayer({
+          id: "seattle-block-outlines",
+          type: "line",
+          source: "seattle-blocks",
+          paint: {
+            "line-color": "#333333",
+            "line-width": 1,
+            "line-opacity": 0.7,
+          },
+        });
       });
     }
 
